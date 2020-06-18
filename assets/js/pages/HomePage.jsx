0885e13d454382296560094dc830fdcfc8ce4751
anthony <shadowtreiize@live.fr>
2020-06-18 14:16:31 +0200

    erreur mot factures remplacé par status

--- assets/js/pages/HomePage.jsx
+++ assets/js/pages/HomePage.jsx
@@ -5,8 +5,8 @@ const HomePage = (props) => {
     <div className="jumbotron">
       <h1 className="display-3">Gestion des factures !</h1>
       <p className="lead">
-        CRM permettant de gérer les factures, les clients et leurs factures via
-        un CRM intéractif et simple d'utilisation.
+        CRM permettant de gérer les factures, les clients et leurs status via un
+        CRM intéractif et simple d'utilisation.
       </p>
       <hr className="my-4" />
       <p>
